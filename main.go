5f7eba6bdf70016e0d978476da2a44b10f2237ad
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-04-04 21:02:28 +0400

    add updates

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tickets here to attend")	
+	var confereceName = "Go Conference"
+	const confereceTikets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", confereceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confereceTikets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend\n")
 }
