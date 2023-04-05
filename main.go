f3c692b19c293314050b8c131fe355d7078ee26d
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-04-05 22:22:07 +0400

    add updates

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 import "fmt"
 
 func main() {
-	var confereceName = "Go Conference"
+	confereceName := "Go Conference"
 	const confereceTikets = 50
-	var remainingTickets = 50
+	remainingTickets := 50
+
+	fmt.Printf("remainingTickets type is %T\n", remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", confereceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confereceTikets, remainingTickets)
-	fmt.Printf("Get your tickets here to attend\n")
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	//some logic
+
+	userName = "Tom"
+	userTickets = 8
+	fmt.Println(userName, userTickets)
 }
