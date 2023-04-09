694a6a9e93a2c2e773965eba8fa07dd1b60b646c
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-04-09 20:46:15 +0400

    Add in conditions and looping a booking operation

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	confereceName := "Go Conference"
 	const confereceTikets = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("remainingTickets type is %T\n", remainingTickets)
 
@@ -12,22 +17,51 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confereceTikets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Please enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("Please enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Please enter how many tikets you wanna book: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Please enter how many tikets you wanna book: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n",firstName, lastName, userTickets, email)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@") && len(email) >= 2
+		isValidTicketAmount := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketAmount {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
+
+			firstNames := []string{}
+
+			for _, booking := range bookings {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			noTicketsCondition := remainingTickets == 0
+
+			if noTicketsCondition {
+				fmt.Printf("All tickets was reserved\n")
+				break
+			}
+
+			fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n",firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confereceName)
+			fmt.Printf("Firstnames of bookings are %v\n", firstNames)
+		} else {
+			fmt.Printf("We only have %v tickets, so you can't book %v tickets \n", remainingTickets, userTickets)
+		}
+	}
 }
